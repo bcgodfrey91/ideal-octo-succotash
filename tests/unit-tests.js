c04d6b7842cc472afcc2bd90d2f3e13cc3895950
Ben Godfrey <bcgodfrey91@gmail.com>
2016-12-21 12:00:56 -0700

    Add testing environment

---
+++ tests/unit-tests.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert
+const client = require('../lib/client.js')
+
+describe('the server exists', function(){
+  it('should return true', function(){
+    assert.equal();
+  });
+
+  
+
+});

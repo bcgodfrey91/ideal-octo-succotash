2f4b8d050d8f0427ce08e60e43ab0a2c5c92f98c
Ben Godfrey <bcgodfrey91@gmail.com>
2016-12-21 09:43:59 -0700

    Connect websockets

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const http = require('http');
 const express = require('express');
 const port = process.env.PORT || 3000;
-
+const socketIo = require('socket.io');
 const app = express();
+const server = http.createServer(app).listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+const io = socketIo(server);
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`)
-})
+});
 
-const server = http.createServer(app).listen(port, () => {
-  console.log(`Listening on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('A user has connected.', io.engine.clientsCount);
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+  });
 });
 
 module.exports = server;
